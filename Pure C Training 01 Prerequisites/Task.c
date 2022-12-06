9d3c5c8bd99eb1b078ed49d22290a2778230137f
FloopyG <36450035+FloopyG@users.noreply.github.com>
2022-12-06 19:51:36 +0200

    Task1

---
+++ Pure C Training 01 Prerequisites/Task.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+  printf("Group 3.\nNazar Ohoniuk.\nnazar.fg@gmail.com\nGitHub:\nhttps://github.com/FloopyG/GL-BaseCamp\nDrive Folder:\nhttps://drive.google.com/drive/u/0/folders/1TFSijC7ERpEFj9szWZktyjPKgVxj4S-n\n");
+
+  return 0;
+}

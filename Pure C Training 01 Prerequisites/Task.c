ea5eba045e6d0aa0d914077981cfaa6640599e77
FloopyG <nazar.fg@gmail.com>
2022-12-07 12:45:23 +0200

    Task1.1

--- Pure C Training 01 Prerequisites/Task.c
+++ Pure C Training 01 Prerequisites/Task.c
@@ -4,7 +4,13 @@
 int main()
 {
 
-  printf("Group 3.\nNazar Ohoniuk.\nnazar.fg@gmail.com\nGitHub:\nhttps://github.com/FloopyG/GL-BaseCamp\nDrive Folder:\nhttps://drive.google.com/drive/u/0/folders/1TFSijC7ERpEFj9szWZktyjPKgVxj4S-n\n");
+  printf("Group 3.\n");
+  printf("Nazar Ohoniuk.\n");
+  printf("nazar.fg@gmail.com\n");
+  printf("GitHub:\n");
+  printf("https://github.com/FloopyG/GL-BaseCamp\n");
+  printf("Drive Folder:\n");
+  printf("https://drive.google.com/drive/u/0/folders/1TFSijC7ERpEFj9szWZktyjPKgVxj4S-n\n");
 
   return 0;
 }

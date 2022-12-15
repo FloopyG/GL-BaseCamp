e1e25051cfff1cd41aeb6042d7cd7076de5c015b
FloopyG <nazar.fg@gmail.com>
2022-12-15 11:41:27 +0200

    Add random number generation for arguments

--- Pure C Training 02 Basic calculations/task.c
+++ Pure C Training 02 Basic calculations/task.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 
-void equation1 (double a) 
+void equation1 (float a) 
 {
-  double result = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(a), 2));
+  float result = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(a), 2));
 
   printf("\nEquation 1 result: %f", result);
 }
 
-void equation2 (double a)
+void equation2 (float a)
 {
-  double result = 2 * sin(a);
+  float result = 2 * sin(a);
 
   printf("\nEquation 2 result: %f", result);
 }
 
+float randNumInRange (double range)
+{
+  return (float)(((double)rand()/(double)(RAND_MAX)) * range * 2) - (float)range;
+}
+
 int main()
 {
+  srand(time(NULL));
 
-  for (int i = 1; i <= 5; ++i)
+  for (int i = 0; i < 5; ++i)
   {
-    printf("\nRun %d", i);
+    float a = randNumInRange(10000);
 
-    equation1(i);
-    equation2(i);
+    printf("\nRun %d", i + 1);
+    printf("\narg: %f", a);
+    
+    equation1(a);
+    equation2(a);
   }
 
-
+  printf("\n\n");
   return 0;
 }

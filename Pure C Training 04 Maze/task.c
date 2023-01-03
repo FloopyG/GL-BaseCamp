fc67037da82c4ae61acd28e6b4729feddf9baa98
FloopyG <nazar.fg@gmail.com>
2023-01-03 16:25:33 +0200

    United function args into struct.

--- Pure C Training 04 Maze/task.c
+++ Pure C Training 04 Maze/task.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #include "maze.h"
 
-typedef enum direction {
+typedef enum direction 
+{
   TOP,
   LEFT,
   RIGHT,
   BOTTOM
 } direction;
 
+typedef struct findPathArgs 
+{
+  int current_X;
+  int current_Y;
+  int exit_X;
+  int exit_Y;
+  direction dir;
+} findPathArgs;
 
 // For more cool colorful console output
 void red ()
@@ -37,65 +46,91 @@ void yellow ()
   Algorithm is only looking for any available path, not the shortest one.
 
 */
-int findPath (int current_X, int current_Y, int exit_X, int exit_Y, direction dir) 
+int findPath (struct findPathArgs *args) 
 {
+  //printf("%d\n x: %d\n y: %d\n", maze[args->current_Y][args->current_X], args->current_X + 1, args->current_Y + 1);
   // Checking if current point is our destination
-  if (current_X == exit_X && current_Y == exit_Y) 
+  if (args->current_X == args->exit_X && args->current_Y == args->exit_Y) 
   {
     return 1;
   }
 
   // Changing current position to 3, to mark passed ways and avoid infinite loops
-  maze[current_Y][current_X] = 3;
+  maze[args->current_Y][args->current_X] = 3;
 
   // To see full algorithm path (x, y format)
-  // printf("%d\n x: %d\n y: %d\n", maze[current_Y][current_X], current_X + 1, current_Y + 1);
 
 
   // Depending from where it came, choose which neigbours must be scanned. Never scannes were it came from.
   // Basically moving our position in all possible ways to find a destination. 
   // If all possible available coordinates were checked returns 0.
   // When we arrive at our destination we change the path values to 2, using backtracking.
-  if (maze[current_Y][current_X - 1] == 0 && dir != LEFT) {
-    if (findPath(current_X - 1, current_Y, exit_X, exit_Y, RIGHT) == 1)
+  if (maze[args->current_Y][args->current_X - 1] == 0 && args->dir != LEFT) {
+    args->current_X -= 1;
+    args->dir = RIGHT;
+    if (findPath(args) == 1)
     {
-      maze[current_Y][current_X - 1] = 2;
+      maze[args->current_Y][args->current_X] = 2;
+      args->current_X += 1;
       return 1;
     }
+    args->current_X += 1;
+    args->dir = LEFT;
   }
-  if (maze[current_Y][current_X + 1] == 0 && dir != RIGHT) {
-    if (findPath(current_X + 1, current_Y, exit_X, exit_Y, LEFT) == 1)
+  if (maze[args->current_Y][args->current_X + 1] == 0 && args->dir != RIGHT) {
+    args->current_X += 1;
+    args->dir = LEFT;
+    if (findPath(args) == 1)
     {
-      maze[current_Y][current_X + 1] = 2;
+      maze[args->current_Y][args->current_X] = 2;
+      args->current_X -= 1;
       return 1;
     }
+    args->current_X -= 1;
+    args->dir = RIGHT;
   }
-  if (maze[current_Y + 1][current_X] == 0 && dir != BOTTOM) {
-    if (findPath(current_X, current_Y + 1, exit_X, exit_Y, TOP) == 1)
+  if (maze[args->current_Y + 1][args->current_X] == 0 && args->dir != BOTTOM) {
+    args->current_Y += 1;
+    args->dir = TOP;
+    if (findPath(args) == 1)
     {
-      maze[current_Y + 1][current_X] = 2;
+      maze[args->current_Y][args->current_X] = 2;
+      args->current_Y -= 1;
       return 1;
     }
+    args->current_Y -= 1;
+    args->dir = BOTTOM;
   }
-  if (maze[current_Y - 1][current_X] == 0 && dir != TOP) {
-    if (findPath(current_X, current_Y - 1, exit_X, exit_Y, BOTTOM) == 1)
+  if (maze[args->current_Y - 1][args->current_X] == 0 && args->dir != TOP) {
+    args->current_Y -= 1;
+    args->dir = BOTTOM;
+    if (findPath(args) == 1)
     {
-      maze[current_Y - 1][current_X] = 2;
+      maze[args->current_Y][args->current_X] = 2;
+      args->current_Y += 1;
       return 1;
     }
+    args->current_Y += 1;
+    args->dir = TOP;
   }
 
   // If we are in the dead-end, then the way was wrong, so we chage value from 3 to 0, using backtraking.
-  maze[current_Y][current_X] = 0;
+  maze[args->current_Y][args->current_X] = 0;
   return 0;
 }
 
 int main()
 {
-
-  findPath(0, 26, 48, 39, TOP);
-
+  struct findPathArgs args;
+  args.current_X = 0;
+  args.current_Y = 26;
+  args.exit_X = 49;
+  args.exit_Y = 9;
+  args.dir = LEFT;
   
+  findPath(&args);
+
+
   for (int i = 0; i < HEIGHT; i++)
   {
     for (int j = 0; j < WIDTH; j++)

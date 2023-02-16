c1a8081ab9d29968ed9a2bd1f38565ed00801255
FloopyG <nazar.fg@gmail.com>
2023-02-16 12:00:32 +0200

    Added new folder for Pure C Tasks

---
+++ Pure C Tasks/Pure C Training 01 Prerequisites/Task.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+  printf("Group 3.\n");
+  printf("Nazar Ohoniuk.\n");
+  printf("nazar.fg@gmail.com\n");
+  printf("GitHub:\n");
+  printf("https://github.com/FloopyG/GL-BaseCamp\n");
+  printf("Drive Folder:\n");
+  printf("https://drive.google.com/drive/u/0/folders/1TFSijC7ERpEFj9szWZktyjPKgVxj4S-n\n");
+
+  return 0;
+}
